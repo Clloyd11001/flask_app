875680811fdd4ad659096bb4951bf0adc426236e
clloyd11001 <clloyd11001@gmail.com>
2024-01-29 07:55:30 -0500

    finished

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-docker run -p 5150:5150 avn-test
+# brilliant idea... just do & to run in parallel (this is probably what Vic was talking about lol)
+docker run -p 5150:5150 avn-test &
 
-xdg-open "http://192.168.50.225:5150"
+# for me
+# xdg-open "http://127.0.0.1:5150"
+# for others
+xdg-open "192.168.50.225:5150"
